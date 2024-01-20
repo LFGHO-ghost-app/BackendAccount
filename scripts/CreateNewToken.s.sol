a139e9990c0a9484192db58b3d5de2f279755be7
Jonathan Díaz <jfdiaz@playvox.com>
2024-01-20 11:58:02 -0500

    Update verify signature and process payment

--- scripts/CreateNewToken.s.sol
+++ scripts/CreateNewToken.s.sol
@@ -7,19 +7,13 @@ import {FactoryAccountWallet} from "../src/FactoryAccountWallet.sol";
 contract DeployTokenWallet is Script {
     function run() public returns (address account) {
         uint256 chainId = 11155111;
-        address tokenAddress = 0xc4bF5CbDaBE595361438F8c6a187bDc330539c60;
-        uint256 tokenId = 1;
         address admin = 0x778F53F8549b2c198dc734cb59dfeDd437496499;
-        FactoryAccountWallet factoryAccountWallet = FactoryAccountWallet(0x6BF708dEb1056D6F134b126fc53EA72686aF9c70);
-        bytes memory encodeData = encodeTokenData(chainId, tokenAddress, tokenId);
+        FactoryAccountWallet factoryAccountWallet = FactoryAccountWallet(0x0A2965a37ffd53cDf6c168B64BC03eF70da772A0);
+        bytes memory encodeData = encodeTokenData(chainId);
         return factoryAccountWallet.createAccount(admin, encodeData);
     }
 
-    function encodeTokenData(
-        uint256 chainId,
-        address tokenAddress,
-        uint256 tokenId
-    ) public pure returns (bytes memory) {
-        return abi.encode(chainId, tokenAddress, tokenId);
+    function encodeTokenData(uint256 chainId) public pure returns (bytes memory) {
+        return abi.encode(chainId);
     }
 }
